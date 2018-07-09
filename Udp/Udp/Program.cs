d6903be59543d8fe5c93191b159c2c08853a14f5
Eugene <chepurko0@gmail.com>
2018-07-09 16:33:05 +0300

    1st

--- Udp/Udp/Program.cs
+++ Udp/Udp/Program.cs
@@ -9,12 +9,11 @@ namespace Udp
     class Program
     {
         static IPAddress remoteAddress;
-        static int remotePort = 2221;
-        static int localPort = 2221;
+        static int remotePort;
+        static int localPort;
         static void Main(string[] args)
         {
-            
-        }
 
+        }
     }
 }
