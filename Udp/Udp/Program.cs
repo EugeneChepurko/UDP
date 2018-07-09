039fffc6e40a0a0a3272289052d66eff1e3fb7f6
Eugene <chepurko0@gmail.com>
2018-07-09 17:55:52 +0300

    save

--- Udp/Udp/Program.cs
+++ Udp/Udp/Program.cs
@@ -11,9 +11,88 @@ namespace Udp
         static IPAddress remoteAddress; // host for sending data
         static int remotePort = 2111;
         static int localPort = 2221;
+        static string userName;
         static void Main(string[] args)
         {
-            Console.WriteLine("gg");
+            try
+            {
+                Console.WriteLine("Enter your name:");
+                userName = Console.ReadLine();
+                remoteAddress = IPAddress.Parse("233.233.233.233");
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void SendMessage()
+        {
+            UdpClient sender = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(remoteAddress, remotePort);
+            try
+            {
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    message = String.Format($"{userName} : {message}");
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    sender.Send(data, data.Length, endPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+        private static void ReceiveMessage()
+        {
+            UdpClient receiver = new UdpClient(localPort);
+            receiver.JoinMulticastGroup(remoteAddress, 11);
+            IPEndPoint remoteIP = null;
+            string localAddress = GetLocalAddress();
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIP);
+                    if (remoteIP.Address.ToString().Equals(localAddress))
+                        continue;
+                    string mess = Encoding.Unicode.GetString(data);
+                    Console.WriteLine(mess);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (remoteIP != null)
+                    remoteIP = null;
+                receiver.Close();
+            }
+        }
+
+        private static string GetLocalAddress()
+        {
+            string localIP = "";
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress item in host.AddressList)
+            {
+                if(item.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = item.ToString();
+                    break;
+                }
+            }
+            return localIP;
         }
     }
 }
